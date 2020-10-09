c99ada23694dd3e4a1c403edbc451ed5e6d60096
Christina <christina.pitch@gmail.com>
2020-10-09 16:28:57 -0600

    initial commit

---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,89 @@
+package com.skilldistillery.filmquery.entities;
+
+import java.util.List;
+
+public class Film {
+	private int id;
+	private String title;
+	private int release_year;
+	private int language_id;
+	private int rental_duration;
+	private double rental_rate;
+	private int length;
+	private double replacement_cost;
+	private String rating;
+	private String special_features;
+	private List<Actor> actors;
+	
+	
+	@Override
+	public String toString() {
+		return "Film [id=" + id + ", title=" + title + ", release_year=" + release_year + ", language_id=" + language_id
+				+ ", rental_duration=" + rental_duration + ", rental_rate=" + rental_rate + ", length=" + length
+				+ ", replacement_cost=" + replacement_cost + ", rating=" + rating + ", special_features="
+				+ special_features + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getRelease_year() {
+		return release_year;
+	}
+	public void setRelease_year(int release_year) {
+		this.release_year = release_year;
+	}
+	public int getLanguage_id() {
+		return language_id;
+	}
+	public void setLanguage_id(int language_id) {
+		this.language_id = language_id;
+	}
+	public int getRental_duration() {
+		return rental_duration;
+	}
+	public void setRental_duration(int rental_duration) {
+		this.rental_duration = rental_duration;
+	}
+	public double getRental_rate() {
+		return rental_rate;
+	}
+	public void setRental_rate(double rental_rate) {
+		this.rental_rate = rental_rate;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public double getReplacement_cost() {
+		return replacement_cost;
+	}
+	public void setReplacement_cost(double replacement_cost) {
+		this.replacement_cost = replacement_cost;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getSpecial_features() {
+		return special_features;
+	}
+	public void setSpecial_features(String special_features) {
+		this.special_features = special_features;
+	}
+	
+	
+	
+}
